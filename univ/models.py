90c101f369bd90345dce24a859abcea83ee6f483
Chinseok Lee <me@askcompany.kr>
2019-08-20 11:07:10 +0900

    Professor 모델 생성 및 admin에 등록

--- univ/models.py
+++ univ/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+# blank/null 은 디폴트로 모두 False
+class Professor(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15,
+                             validators=[
+                                 RegexValidator(r'^010\d{8}$', message='휴대폰 번호를 넣어주세요.')
+                             ])
+    email = models.EmailField(blank=True)
+    department_name = models.CharField(max_length=100, blank=True)
