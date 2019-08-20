015b31e2174ab711d736868da1d7e093d5b7e357
Chinseok Lee <me@askcompany.kr>
2019-08-20 15:29:14 +0900

    React로부터의 Ajax 요청을 허용하기 위해, CORS 설정 추가

--- backend/settings.py
+++ backend/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'univ',  # univ 장고앱을 현재 프로젝트에 등록
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000'
+]
