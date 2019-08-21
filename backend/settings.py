15d0f570d3a4f50d028248edd03f9376fe9102d5
Chinseok Lee <me@askcompany.kr>
2019-08-21 17:12:31 +0900

    django-webpack-loader를 root.html 템플릿에 적용

--- backend/settings.py
+++ backend/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'backend', 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('univ/', include('univ.urls')),
+    re_path('', TemplateView.as_view(template_name='root.html')),
 ]
