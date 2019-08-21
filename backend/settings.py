df5cd9f38269da41d299ffd302ebaca1555b7ca4
Chinseok Lee <me@askcompany.kr>
2019-08-21 19:06:43 +0900

    webpack 빌드 내역이 저장되는 assets 디렉토리에 대해 static 경로에 추가

--- backend/settings.py
+++ backend/settings.py
@@ -124,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'assets'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000'
